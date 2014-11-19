0c6c17d798ccf6f78bcb859003d3a500e45129d6
n-kats <n-kats19890214@hotmail.co.jp>
2014-11-19 23:08:38 +0900

    update xygen.js

--- javascripts/xygen.js
+++ javascripts/xygen.js
@@ -9,25 +9,48 @@ var xyelm = (function () {
                 this.vertexes[i][j] = "";
             }
         }
-        this.edges = new Array(h);
+        this.edges = [];
+        this.edgesMap = new Array(h);
         for (var i = 0; i < h; i++) {
-            this.edges[i] = new Array(w);
+            this.edgesMap[i] = new Array(w);
             for (var j = 0; j < w; j++) {
-                this.edges[i][j] = [];
+                this.edgesMap[i][j] = [];
             }
         }
     }
-    xyelm.prototype.addEdge = function (i, j, type) {
-        this.edges[i][j].push(type);
+    xyelm.prototype.addEdge = function (e) {
+        this.edges.push(e);
+        this.edgesMap[e.from.i][e.from.j].push(e.toArraw());
     };
 
-    xyelm.prototype.deleteEdge = function (i, j, k) {
-        this.edges[i][j].splice(k, 1);
+    xyelm.prototype.deleteEdge = function (e) {
+        var i = 0;
+        while (i < this.edges.length) {
+            if (this.edges[i].eq(e)) {
+                this.edges.splice(i, 1);
+            } else {
+                i++;
+            }
+        }
+        i = 0;
+        while (i < this.edgesMap[e.from.i][e.from.j].length) {
+            if (this.edgesMap[e.from.i][e.from.j][i] == e.toArraw()) {
+                this.edgesMap[e.from.i][e.from.j].splice(i, 1);
+            } else {
+                i++;
+            }
+        }
     };
 
-    xyelm.prototype.setVertex = function (i, j, val) {
-        this.vertexes[i][j] = val;
+    /*
+    public setVertex(i: number, j: number, val: string) {
+    this.vertexes[i][j] = val;
+    }
+    */
+    xyelm.prototype.setVertex = function (v) {
+        this.vertexes[v.i][v.j] = v.toTeX();
     };
+
     xyelm.prototype.deleteVertex = function (i, j) {
         this.vertexes[i][j] = "";
     };
@@ -40,7 +63,7 @@ var xyelm = (function () {
                     str += " & ";
                 }
                 str += this.vertexes[i][j];
-                var eds = this.edges[i][j];
+                var eds = this.edgesMap[i][j];
                 for (var k = 0; k < eds.length; k++) {
                     str += eds[k] + " ";
                 }
@@ -51,4 +74,69 @@ var xyelm = (function () {
         return str;
     };
     return xyelm;
+})();
+
+var vertex = (function () {
+    function vertex(i, j, value) {
+        this.i = i;
+        this.j = j;
+        this.value = value;
+    }
+    vertex.prototype.toTeX = function () {
+        return this.value;
+    };
+    vertex.prototype.clear = function () {
+        this.value = "";
+    };
+    vertex.prototype.eq = function (v) {
+        var flag = (this.i == v.j);
+        flag = flag && (this.j == v.j);
+        flag = flag && (this.value == v.value);
+        return flag;
+    };
+    return vertex;
+})();
+
+var edge = (function () {
+    function edge(from, to, value) {
+        this.from = from;
+        this.to = to;
+        this.value = value;
+        this.options = {};
+    }
+    edge.prototype.eq = function (e) {
+        var flag = this.from.eq(e.from);
+        flag = flag && this.to.eq(e.to);
+        flag = flag && (this.value == e.value);
+        return flag;
+    };
+    edge.prototype.direction = function () {
+        var str = "";
+        var di = this.to.i - this.from.i;
+        if (di > 0) {
+            for (var cnt = 0; cnt < di; cnt++) {
+                str += "d";
+            }
+        } else if (di < 0) {
+            for (var cnt = 0; cnt < -di; cnt++) {
+                str += "u";
+            }
+        }
+        var dj = this.to.j - this.from.j;
+        if (dj > 0) {
+            for (var cnt = 0; cnt < dj; cnt++) {
+                str += "r";
+            }
+        } else if (dj < 0) {
+            for (var cnt = 0; cnt < -dj; cnt++) {
+                str += "l";
+            }
+        }
+        return str;
+    };
+    edge.prototype.toArraw = function () {
+        var str = "\\ar[" + this.direction() + "]" + this.value;
+        return str;
+    };
+    return edge;
 })();
