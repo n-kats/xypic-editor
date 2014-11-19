2eabe16b946720f47019a9996052350dc408b03b
n-kats <n-kats19890214@hotmail.co.jp>
2014-11-19 23:41:08 +0900

    enable add edges

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,18 +2,16 @@ var app = angular.module("xy", []);
 
 app.controller("edgeCtrl", function(){
 	this.edges = [];
-	this.add = function(edge){
-		this.edges.push(edge);
+	this.add = function(x){
+    var from = new vertex(x.from[0], x.from[1], "");
+    var to = new vertex(x.to[0], x.to[1], "");
+    var e = new edge(from,to,x.type || "");
+		this.edges.push(e);
+    xy.addEdge(e);
 	};
 	this.edit = function(index){};
 	this.destroy = function(index){
 		this.edges.splice(index,1);
-	};
-
-	this.show = function(x,y,z){
-		console.log(this.newEdgeFrom);
-		console.log(this.newEdgeTo);
-		console.log(this.newEdgeType);
 	};
 });
 app.controller("newEdgeCtrl", function(){
